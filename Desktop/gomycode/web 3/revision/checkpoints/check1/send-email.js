4671bf8caf3df385a54dc97aaa938db2481cc32b
NinaIndra <sasudest@gmail.com>
2023-06-26 19:18:33 +0100

    hell

--- Desktop/gomycode/web 3/revision/checkpoints/check1/send-email.js
+++ Desktop/gomycode/web 3/revision/checkpoints/check1/send-email.js
@@ -3,15 +3,15 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   service: 'your_email_service_provider',
   auth: {
-    user: 'yana11112.u@gmail.com',
-    pass: 'favbd-Jan11',
+    user: 'ur email',
+    pass: 'password',
   },
 });
 
 function sendEmail() {
   const mailOptions = {
-    from: 'yana11112.u@gmail.com',
-    to: 'sasudest@gmail.com',
+    from: 'ur email',
+    to: 'email to send to ',
     subject: 'Test Email',
     text: 'This is a test email sent using Node.js and nodemailer.',
   };
